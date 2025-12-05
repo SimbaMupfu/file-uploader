692181cc907926ca5b997846a12484407a734e49
SimbaMupfu <scmupfururirwa@gmail.com>
2025-12-05 05:58:37 +0200

    Handle file uploads

--- src/main/java/inc/hustles/sims/controller/FileUploadController.java
+++ src/main/java/inc/hustles/sims/controller/FileUploadController.java
@@ -1,17 +1,50 @@
 package inc.hustles.sims.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 public class FileUploadController {
 
     private static String UPLOADED_FOLDER = "uploads/";
+    private static final Logger logger = LoggerFactory.getLogger(FileUploadController.class);
 
     @GetMapping("/")
     public String index() {
+        logger.info("Index page displayed");
         return "upload";
     }
 
-
+    @PostMapping("/upload")
+    public String singleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            logger.info("file is empty");
+            return "redirect:/uploadStatus";
+        }
+        try {
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+            redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+            logger.info("File uploaded successfully");
+        } catch (IOException e) {
+            logger.info("Error whilst uploading file{}", e.getMessage());
+            e.printStackTrace();
+        }
+        return "redirect:/uploadStatus";
+    }
 }
